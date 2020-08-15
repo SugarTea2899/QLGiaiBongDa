21d6853fd712a80724333ca23d8a63e683227d87
SugarTea2899 <quangthien.tiit@gmail.com>
2020-08-15 13:49:03 +0700

    + Fragment thông số trận đấu.

---
+++ app/src/main/java/com/example/qlgiaibongda/activity/StatisticMatchFragment.java
@@ -0,0 +1,33 @@
+package com.example.qlgiaibongda.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.qlgiaibongda.R;
+
+public class StatisticMatchFragment extends Fragment {
+
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_statistic_match, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getWidget();
+    }
+
+    private void getWidget(){
+
+    }
+}
