b2a531da1be87fc493d597e136bdd20b9437321e
SugarTea2899 <quangthien.tiit@gmail.com>
2020-08-15 15:54:28 +0700

    + Màn hình chờ.

--- app/src/main/java/com/example/qlgiaibongda/activity/MainActivity.java
+++ app/src/main/java/com/example/qlgiaibongda/activity/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
           justTest.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View v) {
-                  Intent intent = new Intent(MainActivity.this, Login.class);
+                  Intent intent = new Intent(MainActivity.this, Splash.class);
                   startActivity(intent);
               }
           });
