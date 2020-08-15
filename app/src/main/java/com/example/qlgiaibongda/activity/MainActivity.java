56a1197185592fbba83c3c3da6ca71e7055af750
SugarTea2899 <quangthien.tiit@gmail.com>
2020-08-15 13:18:09 +0700

    + Fragment xem đội hình ra sân.

--- app/src/main/java/com/example/qlgiaibongda/activity/MainActivity.java
+++ app/src/main/java/com/example/qlgiaibongda/activity/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
           justTest.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View v) {
-                  Intent intent = new Intent(MainActivity.this, SelectedList.class);
+                  Intent intent = new Intent(MainActivity.this, ClubDetail.class);
                   startActivity(intent);
               }
           });
